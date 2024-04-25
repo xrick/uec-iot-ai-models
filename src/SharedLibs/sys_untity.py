6c63c3e4fb1aa43e016181929a9728f115eb1d60
RickLiao <xrickliao@gmail.com>
2024-04-25 14:36:10 +0800

    update

--- src/SharedLibs/sys_untity.py
+++ src/SharedLibs/sys_untity.py
@@ -1,6 +1,7 @@
 # Import the sys module to access system-specific information.
+import os
 import sys
-
+import pathlib
 def checkEndianess():
         # Check if the byte order of the platform is "little" (e.g., Intel, Alpha) and display a corresponding message.
     if sys.byteorder == "little":
@@ -12,5 +13,16 @@ def checkEndianess():
         return 2;
     
     # Display another blank line for clarity.
-    print()
+    print();
+
+def ChkDirAndCreate(dir_path):
+    if not pathlib.Path(dir_path).is_dir():
+        print(f"{dir_path} does not exist, create...");
+        os.mkdir(dir_path);
+        return 1;
+    else:
+        print(f"{dir_path} has existed...")
+        return 2;
+
+
     
