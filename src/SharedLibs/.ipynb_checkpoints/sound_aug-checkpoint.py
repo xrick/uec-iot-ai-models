7bdc7cfdb3acdf6d3effec124e995c2a9a961cff
Jiue-Ren Liao (Rick) <xrickliao@gmail.com>
2024-05-19 17:09:48 +0800

    update

---
+++ src/SharedLibs/.ipynb_checkpoints/sound_aug-checkpoint.py
@@ -0,0 +1,19 @@
+import numpy as np
+import wave
+
+def cal_adjusted_rms(clean_rms, snr):
+    a = float(snr) / 20
+    noise_rms = clean_rms / (10**a) 
+    return noise_rms
+
+def cal_amp(wf):
+"""
+wf is an Wave_read object from wave.open
+"""
+    buffer = wf.readframes(wf.getnframes())
+    # The dtype depends on the value of pulse-code modulation. The int16 is set for 16-bit PCM.
+    amptitude = (np.frombuffer(buffer, dtype="int16")).astype(np.float64)
+    return amptitude
+
+def cal_rms(amp):
+    return np.sqrt(np.mean(np.square(amp), axis=-1))
